c1693b3df442138a2b1d003fd6a2ed74cbbca457
Matteo Ponzini <matteoponzini99@gmail.com>
2022-10-29 15:25:18 +0200

    fix install

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 sudo apt install python3-pip -y
-'export PATH="$HOME/.local/bin:$PATH"' >> ~/.zshrc
+sudo apt-get install python3-venv -y
+python3 -m venv venv
+source ./venv/bin/activate
 pip3 install -r src/requirements.txt
-nohup python3 src/watcher.py &
+./src/watcher.py

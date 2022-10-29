c75d210833687c34f6ee906a0df2fbe7a57def5c
Matteo Ponzini <matteoponzini99@gmail.com>
2022-10-29 15:40:45 +0200

    fix install

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ sudo apt-get install python3-venv -y
 python3 -m venv venv
 source ./venv/bin/activate
 pip3 install -r src/requirements.txt
+ansible-vault create vault.yml
+read -p "Enter password vault: " passwordVault
+echo $passwordVault > password.txt
 ./src/watcher.py
